a87cb8e7d0fc8c4436d06c808ce65beb1295092f
narodeSiddhant <siddhantnarode@gmail.com>
2025-08-06 21:25:08 -0400

    testing

--- src/Labs/Lab5/client.ts
+++ src/Labs/Lab5/client.ts
@@ -1,5 +1,9 @@
 import axios from "axios";
 const REMOTE_SERVER = import.meta.env.VITE_REMOTE_SERVER;
+
+// Add debugging to check if the variable is loaded
+console.log("REMOTE_SERVER:", REMOTE_SERVER);
+
 export const fetchWelcomeMessage = async () => {
   const response = await axios.get(`${REMOTE_SERVER}/lab5/welcome`);
   return response.data;
