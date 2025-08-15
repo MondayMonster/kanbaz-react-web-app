20f22095703363fc5c5b528dba4878fe96506eb1
narodeSiddhant <siddhantnarode@gmail.com>
2025-08-15 12:23:11 -0400

    added quizdeets

--- src/Kanbas/Courses/Quizzes/QuizDetail.tsx
+++ src/Kanbas/Courses/Quizzes/QuizDetail.tsx
@@ -1,5 +1,5 @@
 import  { useEffect, useState } from "react";
-import { useParams, useLocation } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { findQuestionsForQuiz } from "./client";
@@ -12,7 +12,6 @@ const QuizDetail = () => {
   const { currentUser } = useSelector((state: any) => state.accountReducer);
   const { quizzes } = useSelector((state: any) => state.quizzesReducer); // Get quizzes from Redux store
   const [questions, setQuestions] = useState([]);
-  const [isNewQuiz, setIsNewQuiz] = useState(false);
   const [quiz, setQuiz] = useState<any>(null);
 
   // Function to fetch quiz data
@@ -49,7 +48,6 @@ const QuizDetail = () => {
   };
   useEffect(() => {
     if (quizId === "NewQuiz") {
-      setIsNewQuiz(true);
       return;
     }
     const fetchQuestions = async () => {
@@ -65,10 +63,6 @@ const QuizDetail = () => {
   }, [quizId]);
 
   const isFaculty = currentUser.role === "FACULTY"; // Check if the user is a faculty member
-  const totalPoints = questions.reduce(
-    (sum: number, q: any) => sum + q.points,
-    0
-  ); // Sum of all question points
 
   return (
     <div className="container mt-4">
@@ -256,4 +250,3 @@ const QuizDetail = () => {
   );
 };
 
-export default QuizDetail;
