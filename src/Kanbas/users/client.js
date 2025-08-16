37118d5a633ed1e1b45729eae84869f098bb7bfb
narodeSiddhant <siddhantnarode@gmail.com>
2025-08-16 11:51:34 -0400

    updated urls

--- src/Kanbas/users/client.js
+++ src/Kanbas/users/client.js
@@ -2,7 +2,7 @@ import axios from "axios";
 //export const BASE_API = process.env.REACT_APP_BASE_API_URL;
 //export const USERS_API = `${BASE_API}/api/users`;
 //export const BASE_API = "http://localhost:4000";
-export const BASE_API = "https://kanbas-node-server-app-a6-mtju.onrender.com"
+export const BASE_API = "https://kambaz-node-server-app-monday-monster-fp.onrender.com"
 export const USERS_API = `${BASE_API}/api/users`;
 const request = axios.create({
     withCredentials: true,
